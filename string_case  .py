f1882387f473e6af212943a3ed723acaa7116add
cxh214917 <2904504961@qq.com>
2020-11-29 20:32:16 +0800

    1130

---
+++ string_case  .py
@@ -0,0 +1,16 @@
+str1 = 'hello world,bless you'
+# 字符串首单词首字母大写
+str2 = str1.capitalize()
+
+# 字符串所有单词首字母大写
+str3 = str1.title()
+
+# 字符串单词全部小写
+str4 = str1.lower()
+
+# 字符串单词全部大写
+str5 = str1.upper()
+print(str2)
+print(str3)
+print(str4)
+print(str5)
